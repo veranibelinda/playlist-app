0d38906e846c7a83feed264009a9e6d2ec33e0fb
belindaverani <belindaverani1@gmail.com>
2021-09-08 11:59:28 +0700

    menampilkan beberapa lagu

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import data from './components/data/data.js'
 import Track from './components/track';
 
 function App() {
+  const Tracks = () => {
+    return data.map(data => 
+    <Track key={data.id} image={data.album.images[0].url} title={data.name} artist={data.artists[0].name} album={data.album.name} />)
+  }
+
   return (
     <div className="App">
       <h1>PLAYLIST</h1>
-      <Track image={data.album.images[0].url} title={data.name} artist={data.artists[0].name} album={data.album.name} />
+      <Tracks />
     </div>
   );
 }
